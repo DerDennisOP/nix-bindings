90c89bed7d906b60edaf7115de6544c7a75f4099
Dennis Wuitz <dennish@wuitz.de>
2026-06-13 18:03:16 +0200

    build: point dev shell at the local nix fork for eval C-API primitives

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   inputs.nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+  inputs.nix-fork.url = "git+file:///home/dennis/repos/nix?ref=feat/optimized-eval-capi";
+  inputs.nix-fork.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {nixpkgs, nix-fork, ...}: let
     systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
     pkgsForEach = nixpkgs.legacyPackages;
@@ -9,7 +11,10 @@
     devShells = forEachSystem (system: let
       pkgs = pkgsForEach.${system};
     in {
-      default = pkgsForEach.${system}.callPackage ./shell.nix {inherit pkgs;};
+      default = pkgsForEach.${system}.callPackage ./shell.nix {
+          inherit pkgs;
+          nixForBindings = nix-fork.packages.${system}.default;
+        };
     });
   };
 }
